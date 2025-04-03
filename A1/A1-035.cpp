e663ed1b19a89e04a93ec6bbd1216912ae914007
Pasit Sangprachathanarak <ouipingpasitsang@gmail.com>
2025-04-03 10:08:51 +0700

    Update A1-035.cpp

--- A1/A1-035.cpp
+++ A1/A1-035.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){ios::sync_with_stdio(false);, cin.tie(nullptr);
     int n;
     cin >> n;
-    unsigned __int128 a = 0;
-    for(unsigned __int128 i=1; i<=n; i++){
+    unsigned long long a = 0;
+    for(unsigned long long i=1; i<=n; i++){
         a += i*i;
     }
-    cout << (long long)a;
+    cout << (unsigned long long)a;
 }
