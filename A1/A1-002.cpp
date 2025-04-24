04b07182972c9d4d6d50e1799d66046bf60d2d9c
Pasit Sangprachathanarak <ouipingpasitsang@gmail.com>
2025-04-24 21:11:26 +0700

    Format stuff

--- A1/A1-002.cpp
+++ A1/A1-002.cpp
@@ -6,7 +6,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
-    cin >> n;;
+    cin >> n;
     cout << "10 = " << n/10 << '\n';
     n%=10;
     cout << "5 = " << n/5 << '\n';
