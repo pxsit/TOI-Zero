7d5468e98ec9c314f6c76d0906a95d7e8b732b05
Pasit Sangprachathanarak <ouipingpasitsang@gmail.com>
2025-04-27 22:37:42 +0700

    Fix bug

--- A1/A1-005.cpp
+++ A1/A1-005.cpp
@@ -8,7 +8,7 @@ int main(){ios::sync_with_stdio(false);cin.tie(nullptr);
     if(a == 1 || a == 2 || (a == 3 && b < 21))
         cout << "winter";
     else if((a == 3 && b >= 21) || a == 4 || a == 5 || (a == 6 && b < 21))
-        cout << "' 'ring";
+        cout << "spring";
     else if((a == 6 && b >= 21) || a == 7 || a == 8 || (a == 9 && b < 21))
         cout << "summer";
     else if((a == 9 && b >= 21) || a == 10 || a == 11 || (a == 12 && b < 21))
